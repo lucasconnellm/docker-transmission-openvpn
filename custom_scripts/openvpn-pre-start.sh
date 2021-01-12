ea1f4cf0d895e77c07e987caeabf083bc3ad0da7
Lucas Connell <lucasconnellm@gmail.com>
2021-01-12 16:16:46 +0000

    added echo for logging

--- custom_scripts/openvpn-pre-start.sh
+++ custom_scripts/openvpn-pre-start.sh
@@ -8,5 +8,6 @@ protonvpn_ovpn_proto="${PROTONVPN_OVPN_PROTO:-udp}"
 
 rand_ovpn=$(ls $protonvpn_ovpn_loc/us-$protonvpn_ovpn_state_code-*.protonvpn.com.$protonvpn_ovpn_proto.ovpn | xargs shuf -n1 -e)
 
+echo "setting $rand_ovpn to default.ovpn"
 rm $protonvpn_ovpn_loc/default.ovpn
 ln -s $rand_ovpn $protonvpn_ovpn_loc/default.ovpn
