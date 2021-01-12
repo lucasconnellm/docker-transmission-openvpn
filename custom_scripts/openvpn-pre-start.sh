61650dc65b36603578c2bada36ce17f4fc554e30
Lucas Connell <lucasconnellm@gmail.com>
2021-01-12 16:14:19 +0000

    shuffle protonvpn configs on startup

---
+++ custom_scripts/openvpn-pre-start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Mount this script at /scripts at runtime.
+
+# default variables
+protonvpn_ovpn_loc="${PROTONVPN_OVPN_LOC:-/etc/openvpn/protonvpn}"
+protonvpn_ovpn_state_code="${PROTONVPN_OVPN_STATE_CODE:-*}"
+protonvpn_ovpn_proto="${PROTONVPN_OVPN_PROTO:-udp}"
+
+rand_ovpn=$(ls $protonvpn_ovpn_loc/us-$protonvpn_ovpn_state_code-*.protonvpn.com.$protonvpn_ovpn_proto.ovpn | xargs shuf -n1 -e)
+
+rm $protonvpn_ovpn_loc/default.ovpn
+ln -s $rand_ovpn $protonvpn_ovpn_loc/default.ovpn
